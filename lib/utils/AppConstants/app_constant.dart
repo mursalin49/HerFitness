ade93cec3126351bca7783e73fce388def765e5d
mursalin49 <mursalinnirob@gmail.com>
2026-05-16 12:05:01 +0600

    Save current work

--- lib/utils/AppConstants/app_constant.dart
+++ lib/utils/AppConstants/app_constant.dart
@@ -2,7 +2,7 @@ class AppConstants {
   //-------------- base url set here ---------------------//
 
   static const String BASE_URL =
-      "https://subscribers-sunny-jose-forty.trycloudflare.com";
+      "https://twelve-cruz-visitors-luggage.trycloudflare.com";
 
   static const String APP_NAME = 'DefaultAppName';
   static const String Publishable_key = '';
